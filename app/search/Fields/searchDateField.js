a2658b101865bf5d153f73278604041186ec2094
Andrew <amontalto@blissfully.com>
2019-03-13 22:57:37 -0400

    the date search format needed a bit of adjusting

--- app/search/Fields/searchDateField.js
+++ app/search/Fields/searchDateField.js
@@ -1,14 +1,66 @@
 "use strict";
 
-const SearchField = require("./common/searchField");
+const BaseObject = require("../../baseObject");
 
-class SearchDateField extends SearchField {
+class SearchDateField extends BaseObject {
 
     constructor() {
         super();
         this._type = "platformCore";
         this._name = "SearchDateField";
+        this.field = undefined;
+        this.operator = undefined;
+        this.searchValue = undefined;
+    }
+
+    _getSoapType() {
+        return `platformCommon:${this.field}`;
+    }
+
+    _getAttributes() {
+        return {
+            "operator": this.operator,
+            "xsi:type": `${this._type}:${this._name}`,
+        };
+    }
+
+    getNode() {
+
+        const attributes = this._getAttributes();
+        const type = this._getSoapType();
+
+        if (!type) {
+            throw new Error(`Invalid SOAP type ${type}`);
+        }
+
+        if (!this.field) {
+            throw new Error("search criteria field not set");
+        }
+
+        if (!this.operator) {
+            throw new Error("search criteria operator not set");
+        }
+
+        if (!this.searchValue) {
+            throw new Error("search criteria searchValue not set");
+        }
+
+        const node = {};
+
+        node[type] = {};
+
+        if (attributes) {
+            node[type]["$attributes"] = attributes;
+        }
+
+        node[type]["platformCore:searchValue"] = {};
+        node[type]["platformCore:searchValue"]["$attributes"] = {};
+        node[type]["platformCore:searchValue"]["$attributes"]["xsi:type"] = "xsd:dateTime"
+        node[type]["platformCore:searchValue"] = this.searchValue;
+
+        return node;
     }
 }
+
 
 module.exports = SearchDateField;
